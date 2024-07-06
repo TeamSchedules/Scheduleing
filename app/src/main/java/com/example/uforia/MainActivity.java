10bd49e28eb0bd122219cbd2c1a872f36f5117b4
DESKTOP-6OEGPN8\i5 10400F <james451055@gmail.com>
2024-07-07 00:01:27 +0900

    login & register 구현
    issue
    - login 구현 : FirebaseAuth 객체 생성 이후 auth = FirebaseAuth.getInstance(); 즉 객체를 생성하고 instance화 하지 않아서 계속 NullobjectError가 발생했음

--- app/src/main/java/com/example/uforia/MainActivity.java
+++ app/src/main/java/com/example/uforia/MainActivity.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.content.Intent;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+    Button mLoginBtn;
+    Button mRegisterBtn;
 
 
     @Override
@@ -21,20 +35,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button1 = (Button) findViewById(R.id.loginBtn);
-        Button button2 = (Button) findViewById(R.id.regiBtn);
+        mEmailText = (EditText)findViewById(R.id.email_input);
+        mPasswordText = (EditText) findViewById(R.id.password_input);
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        mRegisterBtn = (Button) findViewById(R.id.regiBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        // 로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
-                startActivity(intent);
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(MainActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        // 가입 버튼이 눌리면
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
+                // Intent를 이용해 RegisterActivity 를 호출한다.
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/uforia/RegisterActivity.java
+++ app/src/main/java/com/example/uforia/RegisterActivity.java
@@ -3,25 +3,83 @@ package com.example.uforia;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.content.Intent;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    EditText mEmailText, mPasswordText, mStudentIdText, mName;
+    Button mRegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button button1 = (Button) findViewById(R.id.completeRegi);
+        mRegisterBtn = (Button) findViewById(R.id.completeRegi);
+        mEmailText = findViewById(R.id.email_input);
+        mPasswordText = findViewById(R.id.password_input);
+        mStudentIdText = findViewById(R.id.student_number_input);
+        mName = findViewById(R.id.name_input);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegisterComActivity.class);
-                startActivity(intent);
+                final String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(
+                        RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // 가입 성공시
+                                if(task.isSuccessful()){
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    String email = user.getEmail();
+                                    String uid = user.getUid();
+                                    String name = mName.getText().toString().trim();
+
+                                    // 해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                    HashMap<Object, String> hashMap = new HashMap<>();
+
+                                    hashMap.put("uid",uid);
+                                    hashMap.put("email",email);
+                                    hashMap.put("name",name);
+
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference reference = database.getReference("Users");
+                                    reference.child(uid).setValue(hashMap);
+
+                                    // 가입이 이루어졌을 시 가입 화면을 빠져나감
+                                    Intent intent = new Intent(RegisterActivity.this, RegisterComActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                    Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    Toast.makeText(RegisterActivity.this, "다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                                    return; // 해당 메소드 진행을 멈추고 빠져나감
+                                }
+                            }
+                        }
+                );
             }
         });
     }
